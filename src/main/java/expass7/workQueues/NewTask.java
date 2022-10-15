a11bee915eba776a105915cc5ff783e82114a56e
h579699 <jjonasson@ymail.com>
2022-10-15 11:19:26 +0200

    first commit

---
+++ src/main/java/expass7/workQueues/NewTask.java
@@ -0,0 +1,37 @@
+package expass7.workQueues;
+
+import java.util.Scanner;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+public class NewTask {
+
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+    	if (argv.length < 1) {
+    		argv = new String[1];
+    		Scanner in = new Scanner(System.in);
+    		argv[0] = in.nextLine();
+    		in.close();
+    	}
+    	
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+            String message = String.join(" ", argv);
+
+            channel.basicPublish("", TASK_QUEUE_NAME,
+                    MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+
+}
